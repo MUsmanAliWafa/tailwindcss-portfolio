56e1356e52ebf83f1f586ed9359cc6f7f08a020d
musmanaliwafa <musmanaliwafa@gmail.com>
2024-01-10 01:19:42 +0700

    add to top

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,11 +3,16 @@
 window.onscroll = function () {
   const header = document.querySelector('header');
   const fixNav = header.offsetTop;
+  const toTop =document.querySelector('#to-top');
 
   if(window.pageYOffset>fixNav){
     header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
   }else{
     header.classList.remove('navbar-fixed');
+    toTop.classList.add('hidden');
+    toTop.classList.remove('flex');
   }
 };
 
@@ -19,4 +24,13 @@ hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
   
+});
+
+
+// click diluar hamburger
+window.addEventListener('click', function(e){
+  if(e.target != hamburger && e.target != navMenu){
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
 });
