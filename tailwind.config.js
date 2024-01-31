6bb3fbb98ed59a608f370470a016468df4a64962
musmanaliwafa <musmanaliwafa@gmail.com>
2024-01-31 18:39:45 +0700

    t

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  // content: ["index.html"],
+  content: ["./src/**/*.{html,js}"],
   darkMode: 'class',
   theme: {
     container: {
